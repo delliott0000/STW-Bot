a62eb72d9c3814927838f623d56ba7f2c2db6124
Delliott <dlelliott100@outlook.com>
2023-06-20 02:25:08 +0100

    paginator refactoring

--- components/paginator.py
+++ components/paginator.py
@@ -23,30 +23,30 @@ class Paginator(ui.View):
         self.firs_page.disabled = self.prev_page.disabled = self.current_page == 1
         self.next_page.disabled = self.last_page.disabled = self.current_page == len(self.embeds)
 
-    async def change_page(self, interaction):
+    async def edit_page(self, interaction):
         await interaction.response.defer()
         self.update_buttons()
         await self.interaction.edit_original_response(embed=self.embeds[self.current_page - 1], view=self)
 
-    @ui.button(label='<<', style=ButtonStyle.grey)
+    @ui.button(label='<<')
     async def firs_page(self, interaction, button):
         self.current_page = 1
-        await self.change_page(interaction)
+        await self.edit_page(interaction)
 
     @ui.button(label='<', style=ButtonStyle.blurple)
     async def prev_page(self, interaction, button):
         self.current_page -= 1
-        await self.change_page(interaction)
+        await self.edit_page(interaction)
 
     @ui.button(label='>', style=ButtonStyle.blurple)
     async def next_page(self, interaction, button):
         self.current_page += 1
-        await self.change_page(interaction)
+        await self.edit_page(interaction)
 
-    @ui.button(label='>>', style=ButtonStyle.grey)
+    @ui.button(label='>>')
     async def last_page(self, interaction, button):
         self.current_page = len(self.embeds)
-        await self.change_page(interaction)
+        await self.edit_page(interaction)
 
     async def on_timeout(self):
         for button in self.children:
